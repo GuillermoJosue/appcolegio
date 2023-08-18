76b89bd1bbe4294c498589b116982fe5ad47ce68
Guillermo Becerra <112718027+GuillermoJosue@users.noreply.github.com>
2023-08-17 19:55:52 -0500

    se coloca el codigo para la base de datos en linea

--- conexion.php
+++ conexion.php
@@ -9,5 +9,17 @@ function getConnection(){
     $mysqli = mysqli_connect($servidor,$usuario,$password,$bd) or die("Fallo en la conexion de la BD");
     return $mysqli;
 }
+/*
+//para conectar con la base de datos de 000webhostapp.com
+function getConnection(){
+    $servidor = "localhost";
+    $usuario = "id21158399_root";
+    $password = "Idat123!";
+    $bd = "id21158399_bdidat";
+
+    $mysqli = mysqli_connect($servidor,$usuario,$password,$bd) or die("Fallo en la conexion de la BD");
+    return $mysqli;
+}
+*/
 
 ?>
